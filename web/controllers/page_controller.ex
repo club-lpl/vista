149a838cde23f58d770659bbc50ecf921d0a8a94
James Black <james@james-black.com>
2017-01-05 13:24:24 -0700

    Initial work on routes

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -4,4 +4,8 @@ defmodule Vista.PageController do
   def index(conn, _params) do
     render conn, "index.html"
   end
+
+  def show(conn, %{"id" => id}) do
+    render(conn, "show.json", %{"id" => id})
+  end
 end
--- web/views/page_view.ex
+++ web/views/page_view.ex
@@ -1,3 +1,7 @@
 defmodule Vista.PageView do
   use Vista.Web, :view
+
+  def render("show.json", params) do
+    %{id: Map.get(params, "id")}
+  end
 end
