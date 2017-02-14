bad1754c27a0aaa15fb5dd7c4632f5d8fa640433
ksmithut <ksmith.ut@gmail.com>
2017-02-14 12:59:18 -0700

    updates controller with show method

--- README.md
+++ README.md
@@ -3,3 +3,13 @@
 * Install Phoenix - `mix archive.install https://github.com/phoenixframework/archives/raw/master/phoenix_new.ez`
 * Install [Postgres](https://postgresapp.com/)
 * Install Node >= 5.0
+
+```sh
+# Accept installation of dependencies
+mix deps.get
+npm install
+# Will compile your app
+mix ecto.migrate
+# Start the app
+mix phoenix.server
+```
--- web/controllers/chore_controller.ex
+++ web/controllers/chore_controller.ex
@@ -2,21 +2,39 @@ require IEx
 
 defmodule Vista.ChoreController do
   use Vista.Web, :controller
+  alias Vista.{Chore,Repo}
 
   def index(conn, _params) do
-    chores = Vista.Repo.all(Vista.Chore)
+    chores = Repo.all(Chore)
     IEx.pry()
     render(conn, "index.json", chores: chores)
   end
 
+  def show(conn, %{ "id" => id }) do
+    chore = Repo.get(Chore, id)
+    render(conn, "chore.json", chore: chore)
+  end
+
   def create(conn, params) do
-    %Vista.Chore{}
-    |> Vista.Chore.changeset(params)
-    |> Vista.Repo.insert()
+    %Chore{}
+    |> Chore.changeset(params)
+    |> Repo.insert()
     |> handleInsert(conn)
+  end
+
+  def delete(conn, %{ "id" => id }) do
+    chore = Repo.get!(Chore, id)
+    case Repo.delete(chore) do
+      {:ok, struct}       -> # Deleted with success
+      {:error, changeset} -> # Something went wrong
+    end
   end
 
   defp handleInsert({ :ok, chore }, conn) do
     render(conn, "chore.json", chore: chore)
+  end
+
+  defp handleDelete({ :ok, struct }, conn) do
+
   end
 end
